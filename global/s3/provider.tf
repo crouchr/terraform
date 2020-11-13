56c096072645fa34fe8ca061c070dbfc001bcad8
crouchr <richard.crouch@vonage.com>
2020-11-13 17:19:42 +0000

    First commit of terraform

---
+++ global/s3/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  shared_credentials_file = "/home/crouchr/.aws/credentials"
+  profile = "developmentaws"
+}
---
+++ global/s3/s3.tf
@@ -0,0 +1,30 @@
+# FIXME : Need to add server side encryption to this
+# An S3 bucket to hold remote state for all the stages
+
+//terraform {
+//  backend "s3" {}
+//}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "dev-rch-remote-state-s3"
+
+  # enable once basically working
+  #versioning {
+  #  enabled = true
+  #}
+
+  tags = {
+    Name        = "dev-rch-remote-state-s3"
+    ENVIRONMENT = "DEVELOPMENT"
+    OWNER_TEAM  = "NETWORK"
+    BUILT_BY    = "TERRAFORM"
+    STACK       = "NETWORK"
+    PURPOSE     = "SANDBOX_LAB"
+  }
+
+  #lifecycle {
+  #  prevent_destroy = true
+  #}
+}
+
